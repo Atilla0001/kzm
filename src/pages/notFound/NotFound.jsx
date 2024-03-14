05a4e6a9ef474a694185e819216f52dab60b1d22
Atilla0001 <toulouse663831@gmail.com>
2024-03-14 08:54:18 +0300

    son

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -2,15 +2,18 @@
 import "./Notfound.scss"
 function NotFound() {
   return (
-<div>
-{/*   <div style={{display:"flex", justifyContent:"center",backgroundColor:"gray", height:""}}><h1 style={{}}>404 Page Not Found </h1></div> */}
 
-   <div className="yyy">
-   
-      <img src="../../src/assets/images/aic3.jpg" alt=""  />
-    
+
+<div style={{ maxWidth: "35%", marginLeft: "25%" }}>
+  <div style={{ display: "flex", justifyContent: "center", alignItems: "center", margin: "30px" }}>
+    <p style={{ whiteSpace: "nowrap",paddingLeft:"180px",fontWeight:"bold", fontSize:"100%" }}>404 Page Not Found</p>
+  </div>
+
+  <div className="yyy">
+    <img src="../../src/assets/images/aic3.jpg" alt="" style={{ maxHeight: "400px", width: "auto" }} />
   </div>
 </div>
+
  
 
   
